e029d558b0af012b01c510852cfaf24fe3c6c092
itishprasad30 <itishprasad20@gmail.com>
2022-04-05 10:32:21 +0530

    react 18 updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import "antd/dist/antd.css";
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
